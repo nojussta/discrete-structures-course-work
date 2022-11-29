17b813196de6c8cd47fd932d96cabac75f2a26aa
nojussta <nojus.stasiunas2@gmail.com>
2022-11-29 18:28:14 +0200

    add/changes-to-main

--- main.m
+++ main.m
@@ -3,14 +3,25 @@ clc
 
 % Declaring the graph
 verticeCount = 6;
-matrixArray = [1 2; 1 3; 2 3; 2 5; 3 4; 5 6];
+matrixArray = [1 3; 1 4; 1 6; 2 3; 2 1; 3 4; 5 6];
+
+%3 vertices 1 2; 1 3; 2 3
+
+%6 vertices, 1 3; 1 4; 1 6; 2 3; 2 1; 3 4; 5 6
+
+%9 vertices, 1 2; 1 4; 2 3; 2 5; 3 4; 5 6; 6 4; 6 7; 7 8; 9 6
 
 % Forming graph object
 graph = formGraph(matrixArray, verticeCount);
 plot(graph);
 
+%Start of timer
+tic
+
 % Forming graph bridges
 bridges = formBridges(graph);
+
+timer = toc;
 
 disp("Rasti grafo tiltai: ");
 if(height(bridges)==0)
@@ -18,3 +29,5 @@ if(height(bridges)==0)
 else
 disp(bridges);
 end
+
+disp("Skaičiavimo laikas: " + timer + "sec");
